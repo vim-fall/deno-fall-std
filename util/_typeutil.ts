7d5de44b08b7693f2e1e7885beff49d43f4f2eb7
Alisue <lambdalisue@gmail.com>
2024-11-09 21:18:46 +0900

    docs: Remove unnecessary example that breaks CI

--- util/_typeutil.ts
+++ util/_typeutil.ts
@@ -6,12 +6,6 @@ export type Promish<T> = T | Promise<T>;
 /**
  * Recursively flattens the nested structure of an object type, transforming
  * each property to its resolved (non-nested) type.
- *
- * @example
- * ```ts
- * type Example = { a: { b: { c: string } } };
- * type FlatExample = FlatType<Example>; // { a: { b: { c: string } } }
- * ```
  */
 export type FlatType<T> = T extends Record<PropertyKey, unknown>
   ? { [K in keyof T]: FlatType<T[K]> }
@@ -19,22 +13,12 @@ export type FlatType<T> = T extends Record<PropertyKey, unknown>
 
 /**
  * Extracts the first type in a tuple type.
- *
- * @example
- * ```ts
- * type First = FirstType<[string, number, boolean]>; // string
- * ```
  */
 export type FirstType<T extends unknown[]> = T extends [infer F, ...infer _] ? F
   : never;
 
 /**
  * Extracts the last type in a tuple type.
- *
- * @example
- * ```ts
- * type Last = LastType<[string, number, boolean]>; // boolean
- * ```
  */
 export type LastType<T extends unknown[]> = T extends [...infer _, infer L] ? L
   : never;
