17da474c818c89a18fba70b191c9cebb1d21434b
Alisue <lambdalisue@gmail.com>
2024-11-10 04:12:33 +0900

    feat: add removed `Projector` types

--- projector.ts
+++ projector.ts
@@ -1,6 +1,5 @@
 import type { Denops } from "@denops/std";
 import type { IdItem } from "@vim-fall/core/item";
-import type { Projector, ProjectParams } from "@vim-fall/core/projector";
 
 import type { FirstType, LastType } from "./util/_typeutil.ts";
 import { defineSource, type Source } from "./source.ts";
@@ -11,6 +10,42 @@ import {
   derive,
   deriveArray,
 } from "./util/derivable.ts";
+
+/**
+ * Parameters for projecting items.
+ */
+export type ProjectParams<T> = {
+  /**
+   * Async iterable of items to project.
+   */
+  readonly items: AsyncIterable<IdItem<T>>;
+};
+
+/**
+ * Projector that processes items from the source or curator.
+ *
+ * Generally, the projector has the following two roles:
+ *
+ * 1. Filter items from the source or curator based on criteria other than user input (Filter)
+ * 2. Transform items from the source or curator to adapt the item type for further processing (Modifier)
+ *
+ * In built-in extensions, projectors are categorized into the two types above.
+ */
+export type Projector<T, U = T> = {
+  /**
+   * Projects items for further processing.
+   *
+   * @param denops - The Denops instance.
+   * @param params - Parameters specifying the items to project.
+   * @param options - Additional options, including an abort signal.
+   * @returns An async iterator over the projected `IdItem` elements of type `U`.
+   */
+  project(
+    denops: Denops,
+    params: ProjectParams<T>,
+    options: { signal?: AbortSignal },
+  ): AsyncIterableIterator<IdItem<U>>;
+};
 
 /**
  * Defines a projector responsible for transforming or filtering items.
@@ -103,5 +138,3 @@ export function pipeProjectors<
     }) as R;
   }
 }
-
-export type * from "@vim-fall/core/projector";
