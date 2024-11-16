1ca518c23492db83d34f4ee9b6f37af259cbc1af
Alisue <lambdalisue@gmail.com>
2024-11-16 21:24:23 +0900

    feat: only apply relative path to the value

--- builtin/refiner/relative_path.ts
+++ builtin/refiner/relative_path.ts
@@ -8,28 +8,28 @@ type Detail = {
 };
 
 type DetailAfter = {
-  abspath: string;
+  relpath: string;
+};
+
+export type RelativePathOptions = {
+  base?: string;
 };
 
 /**
- * Creates a Projector that converts file paths to relative paths.
- *
- * This Projector transforms each item's `path` property to a relative path
- * based on the current working directory. It also preserves the original
- * absolute path in a new `abspath` field in `DetailAfter`.
- *
- * @returns A Projector that converts absolute paths to relative paths and includes the absolute path as `abspath`.
+ * Creates a Projector that converts file path in value to relative paths.
  */
-export function relativePath(): Refiner<Detail, DetailAfter> {
+export function relativePath(
+  options: RelativePathOptions = {},
+): Refiner<Detail, DetailAfter> {
   return defineRefiner(
     async function* (denops, { items }, { signal }) {
       // Get the current working directory
-      const cwd = await fn.getcwd(denops);
+      const base = options.base ?? await fn.getcwd(denops);
       signal?.throwIfAborted();
 
       // Convert each item's path to a relative path
       for await (const item of items) {
-        const relpath = relative(cwd, item.detail.path);
+        const relpath = relative(base, item.detail.path);
         const value = item.value.replace(item.detail.path, relpath);
 
         // Yield item with updated relative path and original absolute path
@@ -38,8 +38,7 @@ export function relativePath(): Refiner<Detail, DetailAfter> {
           value,
           detail: {
             ...item.detail,
-            path: relpath,
-            abspath: item.detail.path,
+            relpath,
           },
         };
       }
