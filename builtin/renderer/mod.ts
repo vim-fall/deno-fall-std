9aceadadcb523a076009310ee9639057255e9927
Alisue <lambdalisue@gmail.com>
2024-11-16 20:41:00 +0900

    feat: add `relativePath` renderer

--- builtin/renderer/mod.ts
+++ builtin/renderer/mod.ts
@@ -2,4 +2,5 @@
 export * from "./helptag.ts";
 export * from "./nerdfont.ts";
 export * from "./noop.ts";
+export * from "./relative_path.ts";
 export * from "./smart_path.ts";
---
+++ builtin/renderer/relative_path.ts
@@ -0,0 +1,42 @@
+import * as fn from "@denops/std/function";
+import { relative } from "@std/path/relative";
+
+import { defineRenderer, type Renderer } from "../../renderer.ts";
+
+type Detail = {
+  path: string;
+};
+
+/**
+ * Options for the relativePath renderer.
+ */
+export type RelativePathOptions = {
+  /**
+   * The base directory to calculate the relative path. If not specified, the
+   * current working directory is used.
+   */
+  base?: string;
+};
+
+/**
+ * Creates a Renderer that replace file path in the label to relative paths.
+ *
+ * @returns A Renderer that replace file path in the label to relative paths.
+ */
+export function relativePath(
+  options: RelativePathOptions = {},
+): Renderer<Detail> {
+  return defineRenderer(async (denops, { items }, { signal }) => {
+    // Get the current working directory
+    const base = options.base ?? await fn.getcwd(denops);
+    signal?.throwIfAborted();
+
+    // Convert absolute path in label to relative path
+    items.forEach((item) => {
+      const relpath = relative(base, item.detail.path);
+      if (item.label.includes(item.detail.path)) {
+        item.label = item.label.replace(item.detail.path, relpath);
+      }
+    });
+  });
+}
